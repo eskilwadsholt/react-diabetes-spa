5a40eba1bb75735f9fd437377e7142e916282efb
diabetes-tool <eskilsimonkannewadsholt@gmail.com>
2021-01-11 20:20:37 +0100

    interactive top-menu

--- src/nav/navbars.js
+++ src/nav/navbars.js
@@ -2,6 +2,32 @@ import React from 'react';
 import './navbars.css';
 import logo from './screenshot-32x32';
 
+class Topmenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    }
+  }
+  toggleOpen() {
+    const open = this.state.isOpen;
+    this.setState({
+      isOpen: !open,
+    });
+  }
+  render() {
+    const classNames = "top-menu" + (this.state.isOpen ? " open" : "");
+    return (
+      <div className={classNames}
+        onClick={() => this.toggleOpen()}>
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
+      </div>
+    );
+  }
+}
+
 const Bars = {
 
   Top() {
@@ -16,7 +42,8 @@ const Bars = {
     return (
       <div className="topbar">
         <img src={logo} alt="L" className="logo"/>
-        {dia}{cast}
+        <div className="title">{dia}{cast}</div>
+        <Topmenu/>
       </div>
     );
   },
